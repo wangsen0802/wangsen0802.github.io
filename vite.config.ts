d87dbcf529a179a41a0b4045be2186d389d08d8f
wangsen <2765500856@qq.com>
2025-09-19 14:53:17 +0800

    fix: 🐛 0919wangsen 提交

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    fs: {
+      strict: false
+    }
+  },
+  optimizeDeps: {
+    include: ['vue']
+  }
 })
